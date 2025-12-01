57a0635c35cec3f93c2f863f3c7aadc640413f5a
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-01 17:33:57 +0100

    Improvements day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,26 +1,17 @@
 ﻿var lines = File.ReadAllLines("input.txt");
-var dial = 50; // start position
-Func<int, int> tick_lt = (x) => x - 1;
-Func<int, int> tick_rt = (x) => x + 1;
-var sumpart1 = 0;
-var sumpart2 = 0;
+(int dial, int sumpart1, int sumpart2)  = (50, 0, 0); // start position
 foreach (var line in lines)
 {
     var amount = int.Parse(line[1..]);
-    var tick = tick_lt;
-    if (line[0] == 'R')
-        tick = tick_rt;
-
+    int incr = (line[0] == 'R') ? 1 : -1;
     for (var i = 0; i < amount; i++)
     {
-        dial = tick(dial);
+        dial += incr;
         if (dial % 100 == 0)
             sumpart2++;
     }
-
     if (dial % 100 == 0)
         sumpart1++;
 }
-
 Console.WriteLine("Part 1: " + sumpart1);
 Console.WriteLine("Part 2: " + sumpart2);
