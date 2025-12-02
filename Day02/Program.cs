31496bb3b3eb003d322ee958ae78013bd45006d7
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-02 07:48:30 +0100

    Day 02 some more improvements

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,13 +1,9 @@
 ﻿var lines = File.ReadAllText("input.txt").Split(',').Select((x => x.Split('-')));
-(ulong sumpart1, ulong sumpart2) = (0, 0);
+(long sumpart1, long sumpart2) = (0, 0);
 
 foreach (var line in lines)
 {
-    var from = ulong.Parse(line[0]);
-    var to = ulong.Parse(line[1]);
-
-    List<ulong> pt2matches = [];
-    for (ulong i = from; i <= to; i++)
+    for (long i = long.Parse(line[0]); i <= long.Parse(line[1]); i++)
     {
         string id = i.ToString();
         for (int j = 1; j <= id.Length / 2; j++)
@@ -18,19 +14,16 @@ foreach (var line in lines)
             {
                 if (chunks.Count == 2)
                     sumpart1 += i;
-                pt2matches.Add(i);
+                sumpart2 += i;
+                break;
             }
         }
     }
-
-    foreach (var match in pt2matches.Distinct())
-        sumpart2 += match;
 }
 
 Console.WriteLine($"Part 1: {sumpart1}");
 Console.WriteLine($"Part 2: {sumpart2}");
-
-static IEnumerable<string> Split(string str, int chunkSize)
+IEnumerable<string> Split(string str, int chunkSize)
 {
     return Enumerable.Range(0, str.Length / chunkSize)
         .Select(i => str.Substring(i * chunkSize, chunkSize));
