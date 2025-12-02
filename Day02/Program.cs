b0cb1d522b2f6e0901aa4d44c6e4950197a79f26
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-02 07:20:15 +0100

    Day 2 part 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,23 +1,40 @@
-﻿var lines = File.ReadAllText("sample.txt").Split(',').Select((x => x.Split('-')));
+﻿var lines = File.ReadAllText("input.txt").Split(',').Select((x => x.Split('-')));
 (ulong sumpart1, ulong sumpart2) = (0, 0);
 
 foreach (var line in lines)
 {
-    ulong from = ulong.Parse(line[0]);
-    ulong to = ulong.Parse(line[1]);
+    var from = ulong.Parse(line[0]);
+    var to = ulong.Parse(line[1]);
+
+    List<ulong> pt2matches = [];
     for (ulong i = from; i <= to; i++)
     {
         string id = i.ToString();
-        string firsthalf = id.Substring(0, id.Length/2);
-        string secondhalf = id.Substring(id.Length/2);
+        string firsthalf = id.Substring(0, id.Length / 2);
+        string secondhalf = id.Substring(id.Length / 2);
         if (firsthalf == secondhalf)
             sumpart1 += i;
-        
-        
-        
+
+        for (int j = 1; j <= id.Length / 2; j++)
+        {
+            var chunks = Split(id, j).ToList();
+            bool invalid = chunks.All(x => x == chunks.First());
+            if (invalid && id.Length == chunks.Count * chunks.First().Length)
+            {
+                pt2matches.Add(i);
+            }
+        }
     }
 
+    foreach (var match in pt2matches.Distinct())
+        sumpart2 += match;
 }
 
 Console.WriteLine($"Part 1: {sumpart1}");
 Console.WriteLine($"Part 2: {sumpart2}");
+
+static IEnumerable<string> Split(string str, int chunkSize)
+{
+    return Enumerable.Range(0, str.Length / chunkSize)
+        .Select(i => str.Substring(i * chunkSize, chunkSize));
+}
