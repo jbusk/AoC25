a80f99b1689f8528dd3a568a67cdfa01ea4a1de0
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-03 07:40:43 +0100

    Day 3 improvements

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,26 +1,29 @@
 ﻿var lines = File.ReadAllLines("input.txt");
-(long sumpart1, long sumpart2) = (0, 0);
+var (part1, part2) = (0, 0L);
 
 foreach (var line in lines)
 {
     var digits = line.ToArray().Select(x => int.Parse(x.ToString())).ToList();
-    sumpart1 += CalculateMax(digits, 2);
-    sumpart2 += CalculateMax(digits, 12);
+    part1 += int.Parse(GetBest(line, 2));
+    part2 += long.Parse(GetBest(line, 12));
 }
 
-Console.WriteLine($"Part 1: {sumpart1}");
-Console.WriteLine($"Part 2: {sumpart2}");
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
 
-long CalculateMax(List<int> bank, int slots)
+string GetBest(string bank, int slots)
 {
-    var lastIndex = -1;
-    var selectedBank = new List<int>();
+    var bankarray = bank.ToArray();
+    var bestindex = -1;
+    List<char> best = [];
     while (slots-- > 0)
     {
-        var availableBanks = bank.Skip(lastIndex + 1).Take((bank.Count - (lastIndex + 1)) - slots).ToList();
-        var maxValue = availableBanks.Max();
-        lastIndex = bank.IndexOf(maxValue, lastIndex + 1);
-        selectedBank.Add(maxValue);
+        var length = bankarray.Length - slots;
+        var subBank = bankarray[(bestindex + 1) .. length];
+        var maxValue = subBank.Max();
+        bestindex = subBank.IndexOf(maxValue) + bestindex + 1;
+        best.Add(maxValue);
     }
-    return long.Parse(string.Join("", selectedBank));
+
+    return string.Join("", best);
 }
