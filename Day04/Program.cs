5d5085b713cce8288da364fe558fec92003215d7
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-04 07:12:57 +0100

    Day 04 parts 1 and 2

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,6 +1,57 @@
-﻿var lines = File.ReadAllLines("sample.txt");
-var (part1, part2) = (0, 0);
-foreach (var line in lines)
+﻿using Position = (int x, int y);
+
+var lines = File.ReadAllLines("input.txt");
+var (part2, max) = (0, lines.Length);
+HashSet<Position> grid = [];
+for (int y = 0; y < max; y++)
 {
-    
+    for (int x = 0; x < max; x++)
+    {
+        Position position = new(x, y);
+        var value = lines[x][y];
+        if (value == '@')
+            grid.Add(position);
+        // if (value == '@' && NumberOfNeigbourRolls(position) < 4)
+        //     part1++;
+        // grid[position] = NumberOfNeigbourRolls(position);
+    }
 }
+
+bool once = true;
+while (true)
+{
+    var accessable = grid.Where(x => NumberOfNeigbourRolls(x) < 4).ToHashSet();
+    var count = accessable.Count();
+    if (count == 0)
+        break;
+    part2 += count;
+    if (once)
+    {
+        Console.WriteLine("Part 1: " + count);
+        once = false;
+    }
+    // PrintGrid(accessable);
+    grid.RemoveWhere(x => accessable.Contains(x));
+}
+
+Console.WriteLine("Part 2: " + part2);
+
+int NumberOfNeigbourRolls(Position pos)
+{
+    int count = 0;
+    Position[] relPos =
+    [
+        (0, 1), (1, 0), (0, -1), (-1, 0),
+        (1, 1), (-1, 1), (1, -1), (-1, -1)
+    ];
+    foreach (var rel in relPos)
+    {
+        var newPos = (pos.x + rel.x, pos.y + rel.y);
+        if (InRange(newPos, max) && grid.Contains((newPos.Item1,newPos.Item2)))
+            count++;
+    }
+
+    return count;
+}
+
+bool InRange(Position pos, int max) => (pos.x < max && pos.x >= 0 && pos.y < max && pos.y >= 0);
