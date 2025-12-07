351043e93d79fdb960749b5f82082bfa86576390
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-07 11:32:46 +0100

    Day 7 Tiny optimisation

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,7 +1,7 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 Dictionary<(int x, int y), long> cache = [];
 int part1 = 0;
-Console.WriteLine("Part 2: " + RecurseManifold(1, lines[0].IndexOf('S')));
+Console.WriteLine("Part 2: " + RecurseManifold(2, lines[0].IndexOf('S')));
 Console.WriteLine("Part 1: " + part1);
 
 long RecurseManifold(int x, int y)
@@ -14,10 +14,10 @@ long RecurseManifold(int x, int y)
     if (lines[x][y] is '^') // splitter
     {
         part1++;
-        retval = RecurseManifold(x, y - 1) + RecurseManifold(x, y + 1);
+        retval = RecurseManifold(x + 2, y - 1) + RecurseManifold(x + 2, y + 1);
     }
     else
-        retval = RecurseManifold(x + 1, y);
+        retval = RecurseManifold(x + 2, y);
 
     cache.Add((x, y), retval);
     return retval;
