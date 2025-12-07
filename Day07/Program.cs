3dc349f23e0bd8376d370758d1a26ea5df1aa696
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-07 10:32:13 +0100

    Day 7 cleanup

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,43 +1,24 @@
-﻿using Position = (int x, int y);
+﻿var lines = File.ReadAllLines("input.txt");
+Dictionary<(int x, int y), long> cache = [];
+int part1 = 0;
+Console.WriteLine("Part 2: " + RecurseManifold(1, lines[0].IndexOf('S')));
+Console.WriteLine("Part 1: " + part1);
 
-var lines = File.ReadAllLines("input.txt");
-Dictionary<Position, long> cache = [];
-Position start = (0, lines[0].IndexOf('S'));
-HashSet<Position> beams = [start];
-HashSet<Position> splitters = [];
-
-for (int row = 1; row < lines.Length; row++)
+long RecurseManifold(int x, int y)
 {
-    var line = lines[row];
-    for (int col = 0; col < line.Length; col++)
-        if (beams.Contains(new Position(row - 1, col)))
-        {
-            if (line[col] == '^')
-            {
-                splitters.Add((row, col));
-                Position[] newpositions = [(row, col - 1), (row, col + 1)];
-                foreach (var newpos in newpositions)
-                    beams.Add(newpos);
-            }
-            else
-                beams.Add(new Position(row, col));
-        }
-}
-
-Console.WriteLine("Part 1: " + splitters.Count);
-Console.WriteLine("Part 2: " + RecurseManifold(start));
-
-long RecurseManifold(Position pos)
-{
-    if (cache.TryGetValue(pos, out var value))
+    if (cache.TryGetValue((x, y), out var value))
         return value;
-    if (pos.x == lines.Length)
+    if (x == lines.Length)
         return 1L;
-    var retval = 0L;
-    if (splitters.Contains(pos))
-        retval = RecurseManifold((pos.x, pos.y - 1)) + RecurseManifold((pos.x, pos.y + 1));
+    long retval;
+    if (lines[x][y] is '^') // splitter
+    {
+        part1++;
+        retval = RecurseManifold(x, y - 1) + RecurseManifold(x, y + 1);
+    }
     else
-        retval = RecurseManifold((pos.x + 1, pos.y));
-    cache.Add(pos, retval);
+        retval = RecurseManifold(x + 1, y);
+
+    cache.Add((x, y), retval);
     return retval;
 }
