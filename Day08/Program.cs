6bf689a0f23659c2fd8dd66725b0673c6bb28b61
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-08 18:28:28 +0100

    More day8 cleanup

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -24,11 +24,7 @@ foreach (var b1 in boxes)
             continue;
         if (distances.ContainsKey((b1, b2)) || distances.ContainsKey((b2, b1)))
             continue;
-        distances.Add((b1, b2), Math.Sqrt(
-            Math.Pow(b1.x - b2.x, 2) +
-            Math.Pow(b1.y - b2.y, 2) +
-            Math.Pow(b1.z - b2.z, 2)
-        ));
+        distances.Add((b1, b2), Math.Sqrt(Math.Pow(b1.x - b2.x, 2) + Math.Pow(b1.y - b2.y, 2) + Math.Pow(b1.z - b2.z, 2)));
     }
 }
 
@@ -37,47 +33,36 @@ var connections = distances
     .Select(kv => (kv.Key.Item1, kv.Key.Item2))
     .ToList();
 
-Console.WriteLine("Part 1: " + Part1(connections.Take(iterationsPt1)));
-Console.WriteLine("Part 2: " + Part2(connections, lines.Length));
-
-int Part1(IEnumerable<(Position, Position)> pairs)
+var circuits = new Dictionary<Position, HashSet<Position>>();
+int i = 0;
+foreach (var pair in connections)
 {
-    var circuits = new Dictionary<Position, HashSet<Position>>();
-    foreach (var pair in pairs)
-        ConnectPair(circuits, pair);
-    var sizes = circuits.Values
-        .Select(v => v.Count)
-        .OrderByDescending(c => c)
-        .Distinct()
-        .Take(3);
-    return sizes.Aggregate(1, (a, b) => a * b);
-}
-
-int Part2(IEnumerable<(Position, Position)> pairs, int length)
-{
-    var circuits = new Dictionary<Position, HashSet<Position>>();
-    foreach (var pair in pairs)
-    {
-        ConnectPair(circuits, pair);
-        if (circuits.Count == length)
-            return pair.Item1.x * pair.Item2.x;
-    }
-
-    return int.MinValue;
-}
-
-void ConnectPair(Dictionary<Position, HashSet<Position>> dictionary, (Position, Position) valueTuple)
-{
-    var circuit1 = dictionary.GetValueOrDefault(valueTuple.Item1);
-    var circuit2 = dictionary.GetValueOrDefault(valueTuple.Item2);
+    var circuit1 = circuits.GetValueOrDefault(pair.Item1);
+    var circuit2 = circuits.GetValueOrDefault(pair.Item2);
     var circuit = (circuit1, circuit2) switch
     {
-        (null, null) => new HashSet<Position> { valueTuple.Item1, valueTuple.Item2 },
+        (null, null) => new HashSet<Position> { pair.Item1, pair.Item2 },
         ({ } c1, null) => c1,
         (null, { } c2) => c2,
         ({ } c1, { } c2) => new HashSet<Position>([.. c1, .. c2])
     };
-    circuit.UnionWith([valueTuple.Item1, valueTuple.Item2]);
-    foreach (var b in circuit)
-        dictionary[b] = circuit;
+    circuit.UnionWith([pair.Item1, pair.Item2]);
+    foreach (var b1 in circuit)
+        circuits[b1] = circuit;
+    if (circuits.Count == lines.Length)
+    {
+        Console.WriteLine("Part 2: " + pair.Item1.x * pair.Item2.x);
+        break;
+    }
+
+    i++;
+    if (i == iterationsPt1)
+    {
+        var sizes = circuits.Values
+            .Select(v => v.Count)
+            .OrderByDescending(c => c)
+            .Distinct()
+            .Take(3);
+        Console.WriteLine("Part 1: " + sizes.Aggregate(1, (a, b) => a * b));
+    }
 }
