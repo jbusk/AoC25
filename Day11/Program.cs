bbc061591ab994fb0dbde64d6b803802d43bf9f4
Johan Busk Eriksson <busk@lysator.liu.se>
2025-12-21 12:46:10 +0100

    Day 11 part 1

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,6 +1,34 @@
-﻿var lines = File.ReadAllLines("sample.txt");
+﻿var lines = File.ReadAllLines("input.txt").Select(x => x.Split(": "));
+var nodes = new Dictionary<string, string[]>();
 
 foreach (var line in lines)
 {
-    
+    nodes[line[0]] = line[1].Split(' ');
 }
+
+Console.WriteLine("Part 1: " + Recurse("you", "out"));
+
+int Recurse(string current, string goal)
+{
+    if (current == goal)
+        return 1;
+    int paths = 0;
+    foreach (var node in nodes[current])
+    {
+        paths += Recurse(node, goal);
+    }
+    return paths;
+}
+
+//
+// int Recurse(string current, string goal)
+// {
+//     if (current == goal)
+//         return 1;
+//     int paths = 0;
+//     foreach (var node in nodes[current])
+//     {
+//         paths += Recurse(node, goal);
+//     }
+//     return paths;
+// }
